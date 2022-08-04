bd5f66f0a74ab7a87655b2ac0c03c39e354763e9
amandakicke <amandakicke@gmail.com>
2022-08-04 08:07:39 -0300

    primeiro commit

---
+++ Modulos em Javascript/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Amanda", 41));
+console.log(mostraCidade("Amanda", "Alto Rio Doce"));
+console.log(mostraHobby("Amanda", "Jardinagem"));
